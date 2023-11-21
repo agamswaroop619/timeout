0fc348072a0fb9da71c556ce90716b3d4067b70a
agamswaroop619 <agam.swaroop619@gmail.com>
2023-11-21 16:32:34 +0530

    fix: random image popup working

--- index.js
+++ index.js
@@ -12,29 +12,39 @@ function openNewTab(imageUrl) {
 
 // Function to fetch image URL from an API
 function fetchImageUrl() {
-  const accessKey = '1yMmCBil52fEDEXjurr-nlzYLYueM8MZM48Rj03pjsI'; // Replace with your Unsplash access key
-  const query = 'horror'; // Replace with your desired query
+  const accessKey = '1yMmCBil52fEDEXjurr-nlzYLYueM8MZM48Rj03pjsI'; // Replace with your actual Unsplash access key
+  const query = 'london'; // Replace with your desired query
 
   const requestUrl = `https://api.unsplash.com/search/photos?query=${query}&client_id=${accessKey}`;
 
   fetch(requestUrl)
     .then(response => response.json())
     .then(data => {
-      const imagesArray = data.results; 
-      const randomNumber = Math.floor(Math.random() * imagesArray.length);
-      const imageUrl = imagesArray[randomNumber].urls.regular;
-      openNewTab(imageUrl);
+      const imagesArray = data.results;
+
+      // Check if there are images in the response
+      if (imagesArray.length > 0) {
+        // Pick a random image from the array
+        const randomNumber = Math.floor(Math.random() * imagesArray.length);
+        const imageUrl = imagesArray[randomNumber].urls.regular;
+
+        openNewTab(imageUrl);
+      } else {
+        console.error('No images found in the API response.');
+      }
     })
     .catch(error => console.error('Error fetching image:', error));
 }
 
-// Listen for messages from the popup
+
 chrome.runtime.onMessage.addListener(function(message) {
   if (message.action === 'startTimer') {
     // Start the timer if not already running
     if (!timerInterval) {
+      // Set a fixed interval of 1 hour (in milliseconds)
       let randomnmber =Math.random();
-      timerInterval = setInterval(openNewTab, randomnmber*1000); 
+      const interval = randomnmber*10000;
+      timerInterval = setInterval(fetchImageUrl, interval);
     }
   } else if (message.action === 'stopTimer') {
     // Stop the timer
